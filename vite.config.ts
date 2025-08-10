9a3eab7f29eec3f4094297abff8c1e5dc2bb8408
rafaello129 <rr91175342@gmail.com>
2025-08-10 01:18:21 -0500

    k

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),react()],
+  base: '/', // Para deployment en raíz
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // Directorio para assets
+    emptyOutDir: true,
+  }
 })
